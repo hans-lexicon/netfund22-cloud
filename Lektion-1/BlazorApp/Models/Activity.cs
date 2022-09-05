a2457ee911f5efeaf7c281ec74368c82a1347826
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2022-09-05 23:13:47 +0200

    Lektion 1

---
+++ Lektion-1/BlazorApp/Models/Activity.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp.Models
+{
+    public class Activity
+    {
+        public Guid Id { get; set; }
+        public DateTime Created { get; set; }
+        public string Message { get; set; }
+    }
+}
